6061f51db93d677cff4ef10852ebe51cdd37a596
H0RAC3Z <168603274+H0RAC3Z@users.noreply.github.com>
2024-11-15 17:12:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 // Connect to MongoDB
 // Replace with your MongoDB Atlas connection string if needed
