e8ec658adc6792aa9dedff1050c451e1eb4740cf
coltonbernas <coltonb139@gmail.com>
2024-11-19 17:29:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
 
+
   dropZoneElement.addEventListener("click", () => {
     inputElement.click();
   });
 
+ 
   inputElement.addEventListener("change", (e) => {
     if (inputElement.files.length) {
+      validateInputs();
       updateThumbnail(dropZoneElement, inputElement.files[0]);
     }
   });
 
+ 
   dropZoneElement.addEventListener("dragover", (e) => {
     e.preventDefault();
     dropZoneElement.classList.add("drop-zone--over");
@@ -26,37 +30,13 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     e.preventDefault();
     if (e.dataTransfer.files.length) {
       inputElement.files = e.dataTransfer.files;
+      validateInputs();
       updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
     }
     dropZoneElement.classList.remove("drop-zone--over");
   });
 });
 
-function updateThumbnail(dropZoneElement, file) {
-  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-    dropZoneElement.querySelector(".drop-zone__prompt").remove();
-  }
-
-  if (!thumbnailElement) {
-    thumbnailElement = document.createElement("div");
-    thumbnailElement.classList.add("drop-zone__thumb");
-    dropZoneElement.appendChild(thumbnailElement);
-  }
-
-  thumbnailElement.dataset.label = file.name;
-
-  if (file.type.startsWith("image/")) {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-    };
-  } else {
-    thumbnailElement.style.backgroundImage = null;
-  }
-}
 // Below contains the javascript to get an sku from data.js
 document.addEventListener('DOMContentLoaded', () => {
   const baseUrl = 'http://localhost:3000';
