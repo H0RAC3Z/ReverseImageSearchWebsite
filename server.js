8daa3477b876cf5a6fe6686258bec6f08d747ae0
H0RAC3Z <168603274+H0RAC3Z@users.noreply.github.com>
2024-11-24 13:14:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -37,19 +37,52 @@ app1.get('/api/search', async (req, res) => {
 
   try {
     const tool = await db.collection('ToolsDB').findOne({MPN: MPN });
-
-    
-    
-    console.log("Query result:", tool); 
-
-    if (tool) 
-    {
-      res.json(tool);
-    } 
-    else 
-    {
-      res.status(404).json({ error: 'Tool not found' });
+    if(!tool) {
+      return res.status(404).json({error: 'Tool not found' });
     }
+
+    const mprprice = tool.MPR_price;
+    const jbprice = tool.JB_Price; // why is this one capital p?
+    const tenaquipprice = tool.Tenaquip_price;
+    const imglink = tool.Image_link;
+    const mprlink = tool.MPR_link;
+    const jblink = tool.JB_link;
+    const tenaquiplink = tool.Tenaquip_link;
+
+    function cheapest() {
+      const cheapestPrice = jbprice;
+      const cheapestLink = jblink;
+      const source = "JB";
+
+      if(jbprice <= mprprice && jbprice <= tenaquipprice) {
+        cheapestPrice = jbprice;
+        cheapestLink = jblink;
+        source = "JB";
+      } else if(mprprice <= jbprice && mprprice <= tenaquipprice) {
+        cheapestPrice = mprprice;
+        cheapestLink = mprlink;
+        source = "MPR";
+      } else if(tenaquipprice <= jbprice && tenaquipprice <= mprprice) {
+        cheapestPrice = tenaquipprice;
+        cheapestLink = tenaquiplink;
+        source = "Tenaquip";
+      }
+
+      const result = {
+        imglink: imglink,
+        price: cheapestPrice,
+        link: cheapestLink,
+        source: source
+      };
+
+      return result;
+    }
+
+    const result = cheapest();
+
+    console.log("Query result: ", result.price);
+
+    res.json(result);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Error fetching tool from database' });
